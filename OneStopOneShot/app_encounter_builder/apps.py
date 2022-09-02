e38eb3bc693a3c2e93ce5327165ded4d7bce7403
Stacy Green <stacyleegreen@gmail.com>
2022-09-02 11:02:59 +0700

    Added outline for apps and proj

---
+++ OneStopOneShot/app_encounter_builder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppEncounterBuilderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_encounter_builder'
---
+++ OneStopOneShot/app_map_generator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppMapGeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_map_generator'
