0f2c86fb0af4207f3ec9b46395e930349d61cdee
Stacy Green <stacyleegreen@gmail.com>
2022-09-02 11:29:24 +0700

    Added outline of views

---
+++ OneStopOneShot/app_encounter_builder/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("api/encounters/<int:user_id>/", views.get_saved_encounters, name="get_saved_encounters"),
+    path("api/buildencounter/", views.build_encounter, name="build_encounter"),
+    path("api/encounter/<int:encounter_id>/", views.get_encounter, name="get_encounter"),
+]
--- OneStopOneShot/app_encounter_builder/views.py
+++ OneStopOneShot/app_encounter_builder/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import request, HttpResponse, JsonResponse
+import json
+from django.contrib import auth
 
-# Create your views here.
+
+def index(request):
+
+    return render(request, "app_encounter_builder/index.html")
+
+def build_encounter(request):
+
+    return render(request, "app_encounter_builder/buildEncounter.html")
+
+def get_saved_encounters(request, user_id):
+
+    return
+
+def get_encounter(request, encounter_id):
+
+    return
+
+def login(request):
+    if request.method == "GET":
+        return render(request, "app_encounter_builder/login.html")
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        username = data.get("username", "")
+        password = data.get("password", "")
+
+        user = auth.authenticate(request, username=username, password=password)
+        if user == None:
+            return JsonResponse({"message": "Invalid username or password"})
+        else:
+            auth.login(request, user)
+            return JsonResponse({"message": "Ok"})
