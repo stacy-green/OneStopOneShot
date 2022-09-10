b1ad49fd84bb47f8be2da08fdc6a16036b640645
Stacy Green <stacyleegreen@gmail.com>
2022-09-10 10:42:12 +0700

    Moved map images to media folder, created management command to load maps to database

--- OneStopOneShot/app_map_generator/management/commands/loadmaps.py
+++ OneStopOneShot/app_map_generator/management/commands/loadmaps.py
@@ -4,11 +4,25 @@ from app_map_generator.static.app_map_generator.maps import Small, Medium, Large
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
+        Map.objects.all().delete()
 
-        # for x in range(len(mapsize) // 2))
-        # map = Map()
-        # map.img = location
-        # map.size = Filename
+        for x in range(5):
+            x = str(x)
+            map = Map()
+            map.img = "/images/Small_" + x + ".png"
+            map.size = "Small"
+            map.save()
 
-        pass
+        for x in range(5):
+            x = str(x)
+            map = Map()
+            map.img = "/images/Medium_" + x + ".png"
+            map.size = "Medium"
+            map.save()
 
+        for x in range(5):
+            x = str(x)
+            map = Map()
+            map.img = "/images/Large_" + x + ".png"
+            map.size = "Large"
+            map.save()
