eed327d8b829aa324cbc76f3275836dae345d9f5
Stacy Green <stacyleegreen@gmail.com>
2022-09-10 15:40:41 +0700

    Changed loadmaps command to load 45 maps

--- OneStopOneShot/app_map_generator/management/commands/loadmaps.py
+++ OneStopOneShot/app_map_generator/management/commands/loadmaps.py
@@ -1,26 +1,25 @@
 from django.core.management.base import BaseCommand
 from app_map_generator.models import Map
-from app_map_generator.static.app_map_generator.maps import Small, Medium, Large
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         Map.objects.all().delete()
 
-        for x in range(5):
+        for x in range(15):
             x = str(x)
             map = Map()
             map.img = "/images/Small_" + x + ".png"
             map.size = "Small"
             map.save()
 
-        for x in range(5):
+        for x in range(15):
             x = str(x)
             map = Map()
             map.img = "/images/Medium_" + x + ".png"
             map.size = "Medium"
             map.save()
 
-        for x in range(5):
+        for x in range(15):
             x = str(x)
             map = Map()
             map.img = "/images/Large_" + x + ".png"
