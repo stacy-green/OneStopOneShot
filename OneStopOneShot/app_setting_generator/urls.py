49a42d2c742c99610d781aeaa7861b63fc207298
Stacy Green <stacyleegreen@gmail.com>
2022-09-15 11:23:20 +0700

    fixed required fields in villain form

--- OneStopOneShot/app_setting_generator/urls.py
+++ OneStopOneShot/app_setting_generator/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index-setting"),
     path("createvillain/", views.create_villain, name="create_villain"),
-    path("save/", views.save_villain, name="save_villain"),
+    path("savevillain/", views.save_villain, name="save_villain"),
 
 ]
