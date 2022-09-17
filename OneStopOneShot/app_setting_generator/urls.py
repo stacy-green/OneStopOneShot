c3cfbcb199fb1ed56c7c96a0d16ba61ae82b53e0
Stacy Green <stacyleegreen@gmail.com>
2022-09-17 07:51:19 +0700

    Added view to delete villain from database

--- OneStopOneShot/app_setting_generator/urls.py
+++ OneStopOneShot/app_setting_generator/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("createvillain/", views.create_villain, name="create_villain"),
     path("savevillain/", views.save_villain, name="save_villain"),
     path("detailvillain/<int:villain_id>/", views.detail_villain, name="detail_villain"),
-    # path("deletevillain/<int:villain_id>/")
+    path("deletevillain/<int:villain_id>/", views.delete_villain, name="delete_villain")
 
 ]
