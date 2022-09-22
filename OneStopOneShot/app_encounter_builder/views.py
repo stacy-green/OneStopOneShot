b8a38e63bea1ee7091564687c9c1ee5ac87a2129
Stacy Green <stacyleegreen@gmail.com>
2022-09-22 08:40:09 +0700

    Added security to encounter app

--- OneStopOneShot/app_encounter_builder/views.py
+++ OneStopOneShot/app_encounter_builder/views.py
@@ -13,13 +13,21 @@ def test123(request):
 
 def index(request, portfolio_id):
     portfolio = Portfolio.objects.get(id=portfolio_id)
-    context = {"portfolio": portfolio}
-    return render(request, "app_encounter_builder/index.html", context)
+    if portfolio.user == request.user:
+        context = {"portfolio": portfolio}
+        return render(request, "app_encounter_builder/index.html", context)
+    else:
+        return redirect("portfolio:user_profile")
+
+    
 
 def build_encounter(request, portfolio_id):
     portfolio = Portfolio.objects.get(id=portfolio_id)
-    context = {"portfolio": portfolio}
-    return render(request, "app_encounter_builder/encounter-builder.html", context)
+    if portfolio.user == request.user:
+        context = {"portfolio": portfolio}
+        return render(request, "app_encounter_builder/encounter-builder.html", context)
+    else:
+        return redirect("portfolio:user_profile")
 
 def display_statblock(request):
      if request.method == "GET":
@@ -32,19 +40,22 @@ def display_statblock(request):
 
 def save_encounter(request, portfolio_id):
     if request.method == "GET":
-        encounter = Encounter()
-        encounter.monsters = request.GET.get("mon")
-        encounter.party = request.GET.get("par")
-        encounter.exp = request.GET.get("exp")
-        encounter.adjusted_exp = request.GET.get("adj")
-        encounter.difficulty = request.GET.get("dif")
-        encounter.user = request.user
-        encounter.save()
         portfolio = Portfolio.objects.get(id=portfolio_id)
-        encounter.portfolio = portfolio
-        portfolio.save()
-        encounter.save()
-    return redirect('encounter:index', portfolio_id)
+        if portfolio.user == request.user:
+            encounter = Encounter()
+            encounter.monsters = request.GET.get("mon")
+            encounter.party = request.GET.get("par")
+            encounter.exp = request.GET.get("exp")
+            encounter.adjusted_exp = request.GET.get("adj")
+            encounter.difficulty = request.GET.get("dif")
+            encounter.user = request.user
+            encounter.save()
+            encounter.portfolio = portfolio
+            portfolio.save()
+            encounter.save()
+            return redirect('encounter:index', portfolio_id)
+        else:
+            return redirect("portfolio:user_profile")
 
 def load_encounter(request, portfolio_id):
     user = request.user
@@ -60,20 +71,3 @@ def delete_encounter(request, portfolio_id, encounter_id):
         encounter.delete()
         print("Deleted encounter with id: " + str(encounter_id))
     return HttpResponse("Encounter deleted")
-
-########################################################################################################
-
-# def login(request):
-#     if request.method == "GET":
-#         return render(request, "app_encounter_builder/login.html")
-#     elif request.method == "POST":
-#         data = json.loads(request.body)
-#         username = data.get("username", "")
-#         password = data.get("password", "")
-
-#         user = auth.authenticate(request, username=username, password=password)
-#         if user == None:
-#             return JsonResponse({"message": "Invalid username or password"})
-#         else:
-#             auth.login(request, user)
-#             return JsonResponse({"message": "Ok"})
